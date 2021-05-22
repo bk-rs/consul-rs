993a19ed8cc5d7c3130c6b94e3d35f5f1f0da1b1
vkill <vkill.net@gmail.com>
2021-05-22 16:02:57 +0800

    Update Endpoint

--- consul-api-endpoint/consul-api-endpoint-core/src/lib.rs
+++ consul-api-endpoint/consul-api-endpoint-core/src/lib.rs
@@ -1,18 +1,49 @@
+use std::collections::HashMap;
+
 pub use http::{self, Request, Response};
-use http::{header::ACCEPT, Method, Uri, Version};
+use http::{
+    header::{HeaderMap, ACCEPT},
+    Method, Uri, Version,
+};
+use percent_encoding::{percent_encode, AsciiSet, NON_ALPHANUMERIC};
 use serde::de::DeserializeOwned;
 use serde_json::Value;
 
 pub type Body = Vec<u8>;
+const URL_PERCENT_ENCODE_ASCII_SET: &AsciiSet = &NON_ALPHANUMERIC;
 
 pub trait Endpoint {
     type ResponseOkBody: DeserializeOwned;
 
     fn method(&self) -> Method;
     fn path(&self) -> String;
+    fn query(&self) -> HashMap<String, Vec<String>> {
+        HashMap::new()
+    }
+    fn header(&self) -> HeaderMap {
+        HeaderMap::new()
+    }
 
     fn render_request(&self) -> Result<Request<Body>, EndpointRenderRequestError> {
         let uri: Uri = self.path().parse()?;
+
+        let query = self
+            .query()
+            .into_iter()
+            .map(|x| {
+                x.1.iter()
+                    .map(|v| {
+                        format!(
+                            "{}={}",
+                            x.0,
+                            percent_encode(v.as_bytes(), URL_PERCENT_ENCODE_ASCII_SET).to_string()
+                        )
+                    })
+                    .collect::<Vec<String>>()
+            })
+            .flatten()
+            .collect::<Vec<String>>()
+            .join("&");
 
         let request = Request::builder()
             .method(Method::GET)
